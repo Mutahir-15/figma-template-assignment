4478fe9be2524a75ccdf39589915efdd96019761
Mutahir-15 <mutahir.bin.athar2005@gmail.com>
2024-12-26 00:42:40 +0500

    Updated

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className='bg-[#D9D9D9] h-[88px]'>
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/components/Herosection.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+function Herosection() {
+  return (
+    <main className="max-w-[1280px] mx-auto p-0 md:p-10 px-0 md:px-5 mt-2 md:mt-10">
+      <section className="grid grid-rows-2 md:grid-cols-2 justify-center items-center">
+        {/* Text & Button Section */}
+        <div className="text-left">
+          <h1 className="text-black text-[44px] font-bold">
+            Welcome To Our Website
+          </h1>
+          <p className="text-[32px] font-normal mt-2 md:mt-5">
+            Lorem Ipsum is simply dummy text of
+            <br /> the printing and typesetting industry.
+            <br /> Lorem Ipsum has been the industry's standard
+          </p>
+          <button className="text-[28px] bg-black text-white p-1 md:p-3 px-0 md:px-12 mt-5 md:mt-10">
+            Contact US
+          </button>
+        </div>
+        <div>
+          <Image
+            src={"/images/image.png"}
+            alt="Sample Image"
+            width={500}
+            height={500}
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Herosection;
